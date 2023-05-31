683d36205160eac9ebf0aa02e9a68d5a8b62649b
HuyDuog <linhhee2k@gmail.com>
2023-05-31 14:13:13 +0700

    oke

---
+++ TCL/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TCL.MvcApplication" Language="C#" %>
---
+++ TCL/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TCL.Startup))]
+namespace TCL
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
